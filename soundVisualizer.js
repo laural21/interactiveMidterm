a5f2faba3304cf422302f31f35943e1e908480ea
laural21 <lauralehoczki@gmail.com>
2017-10-22 16:40:21 -0400

    Code for midterm as of 10/22

---
+++ soundVisualizer.js
@@ -0,0 +1,90 @@
+var allShapes = [];
+var happySong;
+var chillSong;
+var energeticSong;
+
+function preload(){
+	happySong = loadSound("happy.mp3");
+	chillSong = loadSound("chill.mp3");
+	energeticSong = loadSound("energetic.mp3");
+}
+
+
+function setup(){
+	// Create new Fast Fourier Transform object
+	fft = new p5.FFT();
+	createCanvas(windowWidth, windowHeight);
+	background(0);
+}
+
+function Shape(r, g, b){
+	// Position, size, color scheme (colors based on mood)
+	this.x = width/2;
+	this.y = height-10;
+
+	this.r = r;
+	this.g = g;
+	this.b = b;
+
+	// Original size
+	this.size = 20;
+
+  	var spectrum = fft.analyze();
+	  noStroke();
+	  fill(0,255,0); // spectrum is green
+	  for (var i = 0; i< spectrum.length; i++){
+	    var x = map(i, 0, spectrum.length, 0, width);
+	    var h = -height + map(spectrum[i], 0, 255, height, 0);
+	    rect(x, height, width / spectrum.length, h )
+	  }
+
+  	var waveform = fft.waveform();
+  	noFill();
+  	beginShape();
+  	stroke(255,0,0); // waveform is red
+  	strokeWeight(1);
+  	for (var i = 0; i< waveform.length; i++){
+    	var x = map(i, 0, waveform.length, 0, width);
+    	var y = map( waveform[i], -1, 1, 0, height);
+    	vertex(x,y);
+  	}
+  	endShape();
+
+	this.moveAndDisplay = function(){
+		// Move with Perlin noise
+		// Change size based on waveform
+		// Add waveform's number to 
+	}
+}
+
+function draw(){
+	if (mood == "happy"){
+		r = random(200, 255);
+		g = random(0, 10);
+		b = random(100, 170);
+		happySong.play();
+	} else if (mood == "chill"){
+		r = random(200, 255);
+		g = random(0, 10);
+		b = random(100, 170);
+		chillSong.play();
+	} else if (mood == "energetic"){
+		r = random(200, 255);
+		g = random(0, 10);
+		b = random(100, 170);
+		energeticSong.play();
+	}
+	
+
+	for(int j = 0; j < 5000; j++){
+		allShapes.push(new Shape(r, g, b));
+	}
+
+	for(var i = 0; i < fire.length; i++){
+		allShapes[i].moveAndDisplay();
+	}
+
+	// Remove "expired" particles from the array
+	allShapes.splice(i, 1);
+	i -= 1;
+}
---
+++ userInterface.js
@@ -0,0 +1,64 @@
+// Computer voice that prompts the user to choose a mood
+var voice;
+
+// Question
+var question = "What mood are you in?";
+
+// Speech input
+var userInput;
+
+// Mood (selected verbally)
+var mood;
+
+function setup(){
+	createCanvas(windowWidth, 100);
+	background(255);
+
+	fill(74, 2, 122);
+	text(question, width/2, 30); //CSS needed, size 30px, font-family?, color?
+	// Create visible buttons which change style when they are verbally selected
+
+	// Create speech to text object
+    userInput = new p5.SpeechRec();
+
+    // Create voice object
+    voice = new p5.Speech();
+
+    // Continuously detect input, change even in the middle of the song
+    userInput.continuous = true;
+
+    // Define a choose mood function
+	userInput.onResult = chooseMood;
+
+    userInput.start();
+}
+
+function draw(){}
+
+function chooseMood(){
+
+	// Set variable for mood
+	var mood;
+
+	// Recognize if the user selected a mood
+	var userWords = userInput.resultString.split(' ');
+	var mostRecentWord = userWords[ userWords.length-1 ];
+
+	// Select mood here; soundVisualizer plays music and does the visuals
+	if(mostRecentWord == "happy"){
+		// Simulate clicking of the "happy" button
+		mood = "happy";
+	} else if (mostRecentWord == "chill"){
+		mood = "chill";
+	} else if (mostRecentWord == "energetic"){
+		mood = "energetic";
+	} else {
+		return;
+	}
+}
+
+// Question only has to be asked once, when page is loaded or reloaded
+// Look for function/workaround for how to perform an action on page loaded
+function pageIsLoaded(){
+	voice.speak(question);
+}
