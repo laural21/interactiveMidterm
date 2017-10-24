3ee95db946fd259d48cf29b21206fc1a5540f48a
laural21 <lauralehoczki@gmail.com>
2017-10-23 20:27:31 -0400

    Sound visualizer w/ Kapp's comments & perlin noise

--- soundVisualizer.js
+++ soundVisualizer.js
@@ -26,88 +26,100 @@ function preload(){
 function setup(){
 	// Create new Fast Fourier Transform object
 	fft = new p5.FFT();
-	createCanvas(windowWidth, windowHeight); // Make it responsive!
-	background(0);
+
+	// CRAIG: This will make your canvas responsive to the screen being resized
+	var theCanvas = createCanvas(800, 600);
+	theCanvas.style('width', '100%');
+	theCanvas.style('height', '100%');
 
 	textFont(myFont);
+
+	// Create speech to text object
+  userInput = new p5.SpeechRec();
+
+  // Create voice object
+  voice = new p5.Speech();
+
+  // Continuously detect input, change even in the middle of the song
+  userInput.continuous = true;
+
+  // Define a choose mood function
+	userInput.onResult = chooseMood;
+
+  userInput.start();
+}
+
+function draw(){
+
+	fill(0);
+	rect(0,0,800,600);
+
 	fill(255, 255, 255);
 	textSize(50);
 	text(question, width/2-250, 50);
 
-	// Create speech to text object
-    userInput = new p5.SpeechRec();
-
-    // Create voice object
-    voice = new p5.Speech();
-
-    // Continuously detect input, change even in the middle of the song
-    userInput.continuous = true;
-
-    // Define a choose mood function
-	userInput.onResult = chooseMood;
-
-    userInput.start();
-}
-
-function draw(){
 	var r, g, b;
 	// Hues of pink and purple
 	if (mood == "happy"){
 		r = random(200, 255);
 		g = random(200, 255);
 		b = random(200, 255);
-		happySong.play();
 
 	// Hues of dark blue
 	} else if (mood == "chill"){
 		r = 0;
 		g = random(50, 100);
 		b = random(100, 255);
-		chillSong.play();
 
 	// Hues of yellow, green
 	} else if (mood == "energetic"){
 		r = random(50, 150);
 		g = 255;
 		b = random(0, 100);
-		energeticSong.play();
 	}
-	
 
-	for(var j = 0; j < 500; j++){
+	// CRAIG: 500 particles is a lot to create each time!
+	for(var j = 0; j < 5; j++){
 		allShapes.push(new Shape(r, g, b));
 	}
 
 	for(var i = 0; i < allShapes.length; i++){
 		allShapes[i].display();
+
+		// CRAIG: essentially turned off the pulsate function
+		// you will have to dig into this a little more to make it do what you want it to do
+		// I wouldn't have each object pull the waveform individually - if you have thousands
+		// of particles this will crash your program. just pull the waveform once and analyze
+		// it - all particles will respond the same way since the waveform doesn't change from/
+		// frame to frame
 		allShapes[i].pulsate();
 		//allShapes[i].move();
 
-		// Remove "expired" particles from the array
-		if(allShapes[i] == true){
+		// CRAIG: Remove "expired" particles from the array
+		// I just made this happen randomly, you need to implement this (or your program will run out of memory and crash)
+		if(random(1) > 0.99){
 			allShapes.splice(i, 1);
 			i -= 1;
 		}
-	}	
+	}
 }
 
 function chooseMood(){
-
-	// Set variable for mood
-	var mood;
-
 	// Recognize if the user selected a mood
 	var userWords = userInput.resultString.split(' ');
 	var mostRecentWord = userWords[ userWords.length-1 ];
-
+	
 	// Select mood here; soundVisualizer plays music and does the visuals
 	if(mostRecentWord == "happy"){
 		// Simulate clicking of the "happy" button
 		mood = "happy";
+		happySong.play();
 	} else if (mostRecentWord == "chill"){
 		mood = "chill";
+		chillSong.play();
 	} else if (mostRecentWord == "energetic"){
 		mood = "energetic";
+		energeticSong.play();
 	} else {
 		return;
 	}
@@ -134,7 +146,7 @@ function Shape(r, g, b){
 	this.vertexThreeY = this.vertexTwoY + (random(-15, 15));
 	this.vertexFourX = this.vertexThreeX + (random(-15, 15));
 	this.vertexFourY = this.vertexThreeY + (random(-15, 15));
-  	
+
 
 	this.display = function(){
 		noStroke();
@@ -149,6 +161,7 @@ function Shape(r, g, b){
 
 	// Change size (pulsate) based on the waveform
 	this.pulsate = function(){
+		/*
 		// Verteces move towards or away from the center of the shape based on the waveform
 		var waveform = fft.waveform(); // get waveform at that instant frame
     	var change = map(waveform, -1, 1, -8, 8);
@@ -160,6 +173,7 @@ function Shape(r, g, b){
   		this.vertexThreeY += change
   		this.vertexFourX += change
   		this.vertexFourY += change
+			*/
   	}
 
 
@@ -167,7 +181,7 @@ function Shape(r, g, b){
 	// Move around the screen with Perlin noise
 
 	// Expire particles
-	// if((this.vertexOneX < 0) || (this.vertexOneY < 0) || (this.vertexTwoX > width) 
+	// if((this.vertexOneX < 0) || (this.vertexOneY < 0) || (this.vertexTwoX > width)
 	// || (this.vertexTwoY < 0)){
 	//	return true;
 	//}
